3e96b4d6f3776e45eeb3a246249e26b42c730bcc
jennmez <jennmez@gmail.com>
2020-03-04 11:21:17 -0600

    initial commit of cart

---
+++ client/components/Cart.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class Cart extends React.Component {
+  // WRITE FUNCTIONS TO HANDLE USER INTERACTION WITH CART
+  //remove item completely
+  //add to quantity of item
+  //subtract from the quantity of item
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h1>Shopping Cart</h1>
+      </div>
+    )
+  }
+}
