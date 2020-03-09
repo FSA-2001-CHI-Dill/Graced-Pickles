162f01635a813acf9f9a652e2789286d0e21c2d9
Arjun Velagapudi <arjunvelagapudi@protonmail.com>
2020-03-09 17:28:16 -0500

    AllPickles updated

--- client/components/AllPickles.js
+++ client/components/AllPickles.js
@@ -1,36 +1,31 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {fetchPickles} from '../store/allPickles'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
-class AllPickles extends Component {
-  componentDidMount() {
-    this.props.loadPickles()
-  }
+import useFetchAllItems from '../hooks/useFetchAllItems.js'
 
-  render() {
-    const {pickles, loading, error} = this.props
+const AllPickles = () => {
+  const [state, setParams] = useFetchAllItems('/api/pickles')
+  // useFetchAllItems takes an endpoint and returns state and setParams
+  // state is an object with status and items
+  // setParams take parameters to filter state
 
-    if (loading) return <h2> Loading </h2>
-    if (error) return <h2> Something went wrong! </h2>
-
-    return pickles.map(pickle => (
-      <div key={pickle.id}>
-        <Link to={`/pickles/${pickle.id}`}> {pickle.title} </Link>$
-        {(pickle.price / 100).toFixed(2)}
-      </div>
-    ))
-  }
+  return (
+    <>
+      {
+        {
+          LOADING: <h1>"Loading"</h1>,
+          ERROR: state.response,
+          SUCCESS: state.response.map(pickle => (
+            <div key={pickle.id}>
+              <Link to={`/pickles/${pickle.id}`}>{pickle.title}</Link>
+              ${pickle.price}
+            </div>
+          ))
+        }[state.status]
+      }
+    </>
+  )
 }
+// now to style
 
-const mapState = state => ({
-  pickles: state.allPickles.pickles,
-  loading: state.allPickles.loading,
-  error: state.allPickles.error
-})
-
-const mapDispatch = dispatch => ({
-  loadPickles: () => dispatch(fetchPickles())
-})
-
-export default connect(mapState, mapDispatch)(AllPickles)
+export default AllPickles
