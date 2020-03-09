7fbd47e9392315719e6524cc5358d720f20da80a
jennmez <jennmez@gmail.com>
2020-03-09 16:58:33 -0500

    updates

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,22 +1,49 @@
 import React, {useEffect} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-// import { fetchAllUsers } from './store/allUsers'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-const AllUsers = props => {
-  // const email = useSelector(state => state.users.email)
-  // const nickname = useSelector(state => state.users.nickname)
+class AllUsers extends React.Component {
+  componentDidMount() {
+    this.props.loadPickles()
+  }
 
-  // const dispatch = useDispatch()
+  render() {
+    const {users} = this.props
 
-  // useEffect(() => {
-  //   dispatch(fetchAllUsers())
-  // }, [])
-
-  return (
-    <div>
-      <h1>List Of All Users</h1>
-    </div>
-  )
+    return users.map(user => (
+      <div key={user.id}>
+        <Link to={`/users/${user.id}`}> {user.email} </Link>${user.nickname}
+      </div>
+    ))
+  }
 }
 
-export default AllUsers
+const mapState = state => ({
+  users: state.allUsers.users
+})
+
+const mapDispatch = dispatch => ({
+  loadUsers: () => dispatch(fetch())
+})
+
+export default connect(mapState, mapDispatch)(AllUsers)
+
+// import {useSelector, useDispatch} from 'react-redux'
+// // import { fetchAllUsers } from './store/allUsers'
+
+// const AllUsers = props => {
+//   // const email = useSelector(state => state.users.email)
+//   // const nickname = useSelector(state => state.users.nickname)
+
+//   // const dispatch = useDispatch()
+
+//   // useEffect(() => {
+//   //   dispatch(fetchAllUsers())
+//   // }, [])
+
+//   return (
+//     <div>
+//       <h1>List Of All Users</h1>
+//     </div>
+//   )
+// }
