d8d8f26bed9924c563e698c1e52333af8ca99bee
jennmez <jennmez@gmail.com>
2020-03-10 16:59:51 -0500

    trying to figure out the components in react

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,34 +3,39 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import Layout from './Layout'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Welcome to Graced Pickles</h1>
-    #blessed
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <Link to="/pickles">View All Pickles</Link>
-          <Link to="/cart">View Your Shopping Cart</Link>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/pickles">View All Pickles</Link>
-          <Link to="/cart">View Your Shopping Cart</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+  <React.Fragment>
+    <Layout>
+      <div>
+        <h1>Welcome to Graced Pickles</h1>
+        #blessed
+        <nav>
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+              <Link to="/pickles">View All Pickles</Link>
+              <Link to="/cart">View Your Shopping Cart</Link>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/pickles">View All Pickles</Link>
+              <Link to="/cart">View Your Shopping Cart</Link>
+            </div>
+          )}
+        </nav>
+        <hr />
+      </div>
+    </Layout>
+  </React.Fragment>
 )
 
 /**
