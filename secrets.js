e807a88dba6a16e90bff33c51bb7dad4de41d1c4
Ji Hye Park <jhp0621@gmail.com>
2020-04-13 18:36:34 -0500

    added google oauth

--- secrets.js
+++ secrets.js
@@ -1,5 +1,15 @@
 const stripeKey = 'sk_test_iIitunOGygrusize5ChoeZtA00VeZ6BYll'
 
+const googleClientId =
+  '310451184796-oofbcjct3fefkcegm2cbllu4clf3blsc.apps.googleusercontent.com'
+
+const googleClientSecret = 'W-GCO9CmEzj5imHejElEjO4-'
+
+const googleCallback = 'http://localhost:8080/auth/google/callback'
+
 process.env.STRIPEKEY = stripeKey
+process.env.GOOGLE_CLIENT_ID = googleClientId
+process.env.GOOGLE_CLIENT_SECRET = googleClientSecret
+process.env.GOOGLE_CALLBACK = googleCallback
 
 module.exports = {stripeKey}
